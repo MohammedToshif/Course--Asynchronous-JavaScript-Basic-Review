79e0876e2b76cd12ab62644953a46c39c678e671
MohammedToshif <toshif92612@gmail.com>
2024-02-19 21:30:35 +0530

    new work

--- 03_Status_Codes.js
+++ 03_Status_Codes.js
@@ -6,10 +6,11 @@ request.addEventListener('readystatechange', () => {
 
     if (request.readyState === 4 && request.status === 200) {
         console.log(request, request.responseText);
-    }   else if(request.readyState === 4) {
+    } else if (request.readyState === 4) {
         console.log('could not fetch the data')
     }
 });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todoss/');
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
 request.send();
+
